e6808f73a2ee2de78c45575f00928c744758f835
mvarilo <miro.varilo@helsinki.fi>
2020-08-07 00:32:14 +0300

    Updates

---
+++ precache-manifest.9f3cfce96aae1b59956f65d79a01dafb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e375a4e8e531ef58d446cd8335b101",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e61dc15ecc7331508d43",
+    "url": "/static/css/main.7d5e1bb5.chunk.css"
+  },
+  {
+    "revision": "9c3a5734abaf79dc30f8",
+    "url": "/static/js/2.91149045.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.91149045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61dc15ecc7331508d43",
+    "url": "/static/js/main.80f9d8cc.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7cdbbdf9f45e519257f9f624a39ce197",
+    "url": "/static/media/contact.7cdbbdf9.svg"
+  },
+  {
+    "revision": "114b0eff68700d7fc20f530d5ff0fa83",
+    "url": "/static/media/design.114b0eff.svg"
+  },
+  {
+    "revision": "cd70b9758f76b20a283be209d637c464",
+    "url": "/static/media/education.cd70b975.svg"
+  },
+  {
+    "revision": "67c84d743c11b6fce6194f2777f75159",
+    "url": "/static/media/helsinki.67c84d74.png"
+  },
+  {
+    "revision": "aafac25a655ce03832044cda66c35380",
+    "url": "/static/media/kurssikartta.aafac25a.png"
+  },
+  {
+    "revision": "24aae6aebe566a7f5dd4246cb1d7e586",
+    "url": "/static/media/person2.24aae6ae.svg"
+  },
+  {
+    "revision": "235d346bf9bdc3eb48e48b22b157f9ad",
+    "url": "/static/media/portfolio.235d346b.svg"
+  },
+  {
+    "revision": "e64641563f2580d1356269a85dfb834f",
+    "url": "/static/media/projects.e6464156.svg"
+  },
+  {
+    "revision": "4f5d3d410f30a8274524ed7a0acbcd5b",
+    "url": "/static/media/web.4f5d3d41.svg"
+  }
+]);
